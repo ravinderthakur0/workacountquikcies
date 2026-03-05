a4c853b8644fda566403d69e142c1033a613316c
work.ravinderthakur <work.ravinderthakur@gmail.com>
2026-03-05 21:29:02 +0000

    Changes before Firebase Studio auto-run

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:myapp/screens/main_shell.dart';
 import 'package:myapp/services/audio_service.dart';
 import 'package:myapp/services/permission_service.dart';
+import 'dart:developer' as developer;
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+  try {
+    final Directory appDocumentDir = await getApplicationDocumentsDirectory();
+    await Hive.initFlutter(appDocumentDir.path);
+  } catch (e, s) {
+    developer.log(
+      'Error initializing Hive',
+      name: 'my_app.main',
+      error: e,
+      stackTrace: s,
+    );
+  }
   await AudioServiceHandler.init();
   await PermissionService.requestPermissions();
   runApp(const MyApp());
